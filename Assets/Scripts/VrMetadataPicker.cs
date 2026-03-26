96c387c13a17bbcaad2c3b6aba6deb54d14d254d
Tim Dodds <timstyler2000@hotmail.com>
2026-03-26 11:59:40 +1100

    Initial work on metadata picking with VR inputs

---
+++ Assets/Scripts/VrMetadataPicker.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using System.Collections.Generic;
+using CesiumForUnity;
+using System;
+using System.Text;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VrMetadataPicker : MonoBehaviour
+{
+    [SerializeField] private Transform leftControllerTransform = null;
+    [SerializeField] private Transform rightControllerTransform = null;
+    
+    // Cached Dictionary of metadata values. This prevents reallocation every
+    // time metadata is sampled from the tileset.
+    private Dictionary<String, CesiumMetadataValue> metadataValues = null;
+    private VrControls vrControls = null;
+    private string hoveredMetadata = string.Empty;
+    private string selectedMetadata = string.Empty;
+    
+    private const float RAYCAST_DISTANCE = 1000.0f;
+
+    private void Awake()
+    {
+        vrControls = new VrControls();
+    }
+    
+    private void OnEnable()
+    {
+        vrControls.XRILeftInteraction.Activate.performed += OnTriggerPress;
+        vrControls.XRIRightInteraction.Activate.performed += OnTriggerPress;
+        vrControls.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        vrControls.XRILeftInteraction.Activate.performed -= OnTriggerPress;
+        vrControls.XRIRightInteraction.Activate.performed -= OnTriggerPress;
+        vrControls.Disable();
+    }
+    
+    private void Start()
+    {
+        metadataValues = new Dictionary<String, CesiumMetadataValue>();
+    }
+    
+    // We might need separate left/right functions if we need to distinguish between the 
+    // two controller inputs, for some reason.
+    private void OnTriggerPress(InputAction.CallbackContext obj)
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) return;      // ignore input when hovering over UI object
+        if (hoveredMetadata == string.Empty) return;
+        selectedMetadata = hoveredMetadata;
+        Debug.Log($"Selected: {selectedMetadata}!");
+    }
+    
+    // Right now, both controllers are raycasting all the time, so if the user is waving
+    // both around the metadata will change a lot. We could perhaps only use one controller
+    // for metadata hovering/picking (and maybe other related stuff like building 'level selection')
+    // and the other controller for things like player movement. Or perhaps searching for metadata
+    // could require a button press-hold, and then some other input for selecting (release, or button press).
+    private void Update()
+    {
+        if (leftControllerTransform) RayCastForMetadata(leftControllerTransform);
+        if (rightControllerTransform) RayCastForMetadata(rightControllerTransform);
+    }
+    
+    private void RayCastForMetadata(Transform controllerTransform)
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(controllerTransform.position, controllerTransform.TransformDirection(Vector3.forward), out hit, RAYCAST_DISTANCE))
+        {
+            CesiumPrimitiveFeatures features = hit.transform.GetComponent<CesiumPrimitiveFeatures>();
+            CesiumModelMetadata metadata = hit.transform.GetComponentInParent<CesiumModelMetadata>();
+            
+            if (features && features.featureIdSets.Length > 0)
+            {
+                CesiumFeatureIdSet featureIdSet = features.featureIdSets[0];
+                Int64 propertyTableIndex = featureIdSet.propertyTableIndex;
+                if (metadata && propertyTableIndex >= 0 && propertyTableIndex < metadata.propertyTables.Length)
+                {
+                    CesiumPropertyTable propertyTable = metadata.propertyTables[propertyTableIndex];
+                    Int64 featureID = featureIdSet.GetFeatureIdFromRaycastHit(hit);
+                    propertyTable.GetMetadataValuesForFeature(metadataValues, featureID);
+                }
+                
+                StringBuilder sb = new();
+                
+                foreach (var valuePair in metadataValues)
+                {
+                    string valueAsString = valuePair.Value.GetString();
+                    if (string.IsNullOrEmpty(valueAsString) || valueAsString == "null") continue;
+                    sb.Append($"<b>{valuePair.Key}</b>: {valueAsString}");
+                    sb.AppendLine();
+                }
+                
+                hoveredMetadata = sb.ToString();
+                Debug.Log($"Hovering on: {hoveredMetadata}!");
+                return;
+            }
+        }
+        
+        hoveredMetadata = string.Empty;
+    }
+}
