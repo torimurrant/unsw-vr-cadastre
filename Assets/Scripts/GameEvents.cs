35d9cf1844d6c8ca51c3f2f76a37e7ff78bdc37b
victoria <133925604+torimurrant@users.noreply.github.com>
2026-07-14 19:55:23 +1000

    Remove summary comments in GameEvents.cs
    Removed XML summary comments from GameEvents class.

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -4,24 +4,12 @@ using UnityEngine;
 
 public static class GameEvents
 {
-    /// <summary>
-    /// Caller: VrMetadataPicker
-    /// Listeners: ModelMetadataVisuals
-    /// </summary>
     public static event Action<CesiumModelMetadata> onModelMetadataHoverOn;
     public static void OnModelMetadataHoverOn(CesiumModelMetadata metadata) { onModelMetadataHoverOn?.Invoke(metadata); }
     
-    /// <summary>
-    /// Caller: VrMetadataPicker
-    /// Listeners: ModelMetadataVisuals
-    /// </summary>
     public static event Action onModelMetadataHoverOffAll;
     public static void OnModelMetadataHoverOffAll() { onModelMetadataHoverOffAll?.Invoke(); }
 
-    /// <summary>
-    /// Caller: VrMetadataPicker
-    /// Listeners: CesiumTilesetHider
-    /// </summary>
     public static event Action<TileType> onTileViewChanged;
     public static void OnTileViewChanged(TileType tileType) { onTileViewChanged?.Invoke(tileType); }
 }
